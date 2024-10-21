8de8f6eb5c27de7dd6edbc2dfaf3feb1fe7c481c
AsemDiab <137905232+AsemDiab@users.noreply.github.com>
2024-10-21 20:38:34 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const CATALOG_SERVICE_URL = 'http://localhost:5001';
+const ORDER_SERVICE_URL = 'http://localhost:5002';
+
+// Search for books by topic
+app.get('/search/:topic', async (req, res) => {
+    const topic = req.params.topic;
+    const response = await axios.get(`${CATALOG_SERVICE_URL}/search/${topic}`);
+    res.json(response.data);
+});
+
+// Get details of a specific book
+app.get('/info/:item_number', async (req, res) => {
+    const itemNumber = req.params.item_number;
+    const response = await axios.get(`${CATALOG_SERVICE_URL}/info/${itemNumber}`);
+    res.json(response.data);
+});
+
+// Purchase a book
+app.post('/purchase/:item_number', async (req, res) => {
+    const itemNumber = req.params.item_number;
+    const response = await axios.post(`${ORDER_SERVICE_URL}/purchase/${itemNumber}`,
+        req.body
+    );
+    res.json(response.data);
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Front-End Service running on port ${PORT}`);
+});
